66c316bde553a5b838378e024e991890e49cc628
silentxrture <54028126+silentxrture@users.noreply.github.com>
2023-10-16 22:36:03 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -16,13 +16,13 @@
 
 
 # 修改默认IP为 192.168.69.1
-sed -i 's/192.168.1.1/192.168.69.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.69.1/g' openwrt/package/base-files/files/bin/config_generate
  
 # 修改默认主机名
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='SheepNet'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='SheepNet'' openwrt/package/lean/default-settings/files/zzz-default-settings
  
 # 加入编译者信息
-sed -i "s/OpenWrt /smrtfish build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /smrtfish build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
  
 # 修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" openwrt/feeds/luci/collections/luci/Makefile
