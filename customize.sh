cd6b0a5d2640b3e04b3497746aa515bb0d753e69
silentxrture <54028126+silentxrture@users.noreply.github.com>
2023-10-16 21:45:57 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,10 +6,23 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
+
+
+# 修改默认IP为 192.168.69.1
+sed -i 's/192.168.1.1/192.168.69.1/g' package/base-files/files/bin/config_generate
+ 
+# 修改默认主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='SheepNet'' package/lean/default-settings/files/zzz-default-settings
+ 
+# 加入编译者信息
+sed -i "s/OpenWrt /smrtfish build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+ 
+# 修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
